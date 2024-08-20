b2e67b8c56f0e4d39b293efc893c777f87746dcc
ParamThakkar123 <paramthakkar864@gmail.com>
2024-08-20 13:23:30 +0530

    Added some files for roadmap

---
+++ app/api/ai/ask.ts
@@ -0,0 +1,2 @@
+import { NextRequest, NextResponse } from "next/server";
+import {HumanMessage, SystemMessage} from "@langchain/core/messages"
---
+++ app/lib/generate.ts
@@ -0,0 +1,3 @@
+import { BaseChain, loadQAMapReduceChain } from "langchain/chains" // LLM Chain to be used here but it's deprecated looking for alternative
+import { Document } from "langchain/document"
+import { TextLoader } from "langchain/document_loaders/fs/text"
