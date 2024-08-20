02b0fa20ca6ca83518414505a80ba5cab5605e2a
ParamThakkar123 <paramthakkar864@gmail.com>
2024-08-21 00:44:57 +0530

    Roadmap Code

--- app/api/ai/ask.ts
+++
@@ -1,2 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-import {HumanMessage, SystemMessage} from "@langchain/core/messages"
--- app/lib/generate.ts
+++ app/lib/generate.ts
@@ -1,3 +1,42 @@
-import { BaseChain, loadQAMapReduceChain } from "langchain/chains" // LLM Chain to be used here but it's deprecated looking for alternative
-import { Document } from "langchain/document"
-import { TextLoader } from "langchain/document_loaders/fs/text"
+import { PromptTemplate } from "@langchain/core/prompts"
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai"
+
+export const generate = async ({ input, selectedTemplate }: any) => {
+    try{
+        const model = new ChatGoogleGenerativeAI({
+            model: 'gemini-pro',
+            apiKey: process.env.GEMINI_API_KEY,
+            temperature: 0.9,
+        });
+        const template = 
+        "{syntax} - {instructions} learn from syntax above and write {template} in mermaid syntax about {input}?";
+        const prompt = new PromptTemplate({
+            template,
+            inputVariables: ["template", "input", "syntax", "instructions"],
+        });
+
+        const chain = prompt.pipe(model);
+
+        //@ts-ignore
+        const synataxDoc = await import(
+            `./syntax/${selectedTemplate.toLowerCase()}.md`
+        );
+
+        const res = await chain.invoke({
+            template: selectedTemplate,
+            input: input,
+            syntax: synataxDoc.default,
+            instructions: `
+            - use different shapes, colors and also use icons when possible as mentioned in the doc.
+            - strict rules: do not add Note and do not explain the code and do not add any additional text except code, 
+            - do not use 'end' syntax
+            - do not use any parenthesis inside block
+            `,
+        });
+
+        return res
+    }catch(error){
+        console.log(error);
+        throw error;
+    }
+}
